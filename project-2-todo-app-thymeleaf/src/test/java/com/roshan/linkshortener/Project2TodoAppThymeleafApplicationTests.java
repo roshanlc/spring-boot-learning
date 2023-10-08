c62671e9486a4e63bd21d0efef1d3692087e2e98
Roshan Lamichhane <lamichhanersn@gmail.com>
2023-10-08 13:50:57 +0545

    initial setup for project-2

---
+++ project-2-todo-app-thymeleaf/src/test/java/com/roshan/linkshortener/Project2TodoAppThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshan.linkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2TodoAppThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
