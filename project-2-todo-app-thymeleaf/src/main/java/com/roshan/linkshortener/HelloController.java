628c86d3d2f82f06f48eee4330a44c565743770e
Roshan Lamichhane <lamichhanersn@gmail.com>
2023-10-10 17:59:15 +0545

    Added endpoint to create short links

--- project-2-todo-app-thymeleaf/src/main/java/com/roshan/linkshortener/HelloController.java
+++ project-2-todo-app-thymeleaf/src/main/java/com/roshan/linkshortener/HelloController.java
@@ -2,14 +2,12 @@ package com.roshan.linkshortener;
 
 
 import com.roshan.linkshortener.models.Link;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
+import com.roshan.linkshortener.utilities.ShortName;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -27,11 +25,17 @@ public class HelloController {
     }
 
     @PostMapping(value = "/")
-    public Object addNewLink(@RequestParam(value = "link", required = true) String link) {
-
-        return ":ok";
+    public Link addNewLink(@RequestParam(value = "link", required = true) String link,HttpServletResponse response) {
+        try {
+            String shortLink = ShortName.generateShortLink(link);
+            template.opsForValue().set(shortLink, link);
+            return new Link(shortLink,link);
+        }
+        catch (Exception e){
+            response.setStatus(500);
+            return new Link(null,link);
+        }
     }
-
 
 }
 
