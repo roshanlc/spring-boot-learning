827c656a570ebdf1cef7c780b9d4efcc724591d1
Roshan Lamichhane <lamichhanersn@gmail.com>
2023-10-18 19:14:52 +0545

    project-5: initial backend setup

---
+++ project-5-tasks-mgmt-web-app/task-mgmt-backend/src/main/java/com/roshan/taskmgmt/TaskMgmtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roshan.taskmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMgmtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMgmtBackendApplication.class, args);
+    }
+
+}
---
+++ project-5-tasks-mgmt-web-app/task-mgmt-backend/src/test/java/com/roshan/taskmgmt/TaskMgmtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshan.taskmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMgmtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
